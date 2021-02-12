b938da13a66bd73737fb076247b14d5f3257adb4
ClaudiaJue <73588196+ClaudiaJue@users.noreply.github.com>
2021-02-12 19:42:51 +0100

    Gradle added

--- src/com/calculator/CalcModel.java
+++ src/com/calculator/CalcModel.java
@@ -1,11 +1,12 @@
 package com.calculator;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Getter
 @Setter
 public class CalcModel {
-
     private String operation;
     private String firstNum;
     private String secondNum;
-
 }
